781f203c8b685f52e631c1ea275a07c428a3c9ee
Faiz Zubair <faizvk14@gmail.com>
2026-01-30 13:35:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 function curry(callback) {
-  
+  return function curried(...args) {
+    // If called with no arguments, invoke callback with what we have so far
+    if (args.length === 0) {
+      return callback();
+    }
+
+    let collected = [...args];
+
+    function next(...more) {
+      // Termination condition
+      if (more.length === 0) {
+        return callback(...collected);
+      }
+
+      // Accumulate and keep going
+      collected.push(...more);
+      return next;
+    }
+
+    return next;
+  };
 }
 
-module.export=curry
+module.exports = curry;
